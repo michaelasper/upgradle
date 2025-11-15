44be9fd2676b7a5a4cdbe236fb9ab95768165b6e
Michael Asper <michaelksasper@gmail.com>
2025-11-14 22:45:57 -0600

    fix ico

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const isCI = process.env.GITHUB_ACTIONS === 'true'
+const base = isCI && repoName ? `/${repoName}/` : '/'
+
 export default defineConfig({
+  base,
   plugins: [react()],
 })
